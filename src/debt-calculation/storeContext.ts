041d2a27ac86c5c06e6126482fa582645c166c47
Matej Sajgal <roarbb@gmail.com>
2024-07-02 20:20:07 +1000

    chore: Rename ExampleStore to DebtStore in storeContext.ts

--- src/debt-calculation/storeContext.ts
+++ src/debt-calculation/storeContext.ts
@@ -1,16 +1,16 @@
 import { createContext, useContext, useRef } from 'react';
 import { createDebtStore } from './store';
 
-export type ExampleStore = ReturnType<typeof createDebtStore>;
+export type DebtStore = ReturnType<typeof createDebtStore>;
 
-export const DebtStoreContext = createContext({} as ExampleStore);
+export const DebtStoreContext = createContext({} as DebtStore);
 
 export function useDebtStore() {
   return useContext(DebtStoreContext);
 }
 
 export function useCreateDebtStore() {
-  const storeRef = useRef<ExampleStore>();
+  const storeRef = useRef<DebtStore>();
 
   if (storeRef.current === undefined) {
     // can pass optional props
